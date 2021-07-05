48b7dfefb87f6bf09e5c7808b5bd55e9f7d0ac7c
SerhioAndros <androsiuksv@gmail.com>
2021-07-05 03:20:12 +0300

    added local storage

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,11 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import contactsReducer from "./contacts-reducer";
 
+const contactsPersistConfig = {
+  key: "userContacts",
+  storage,
+  blacklist: ["filter"],
+};
+
 const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(contactsPersistConfig, contactsReducer),
   },
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export default { store, persistor };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import store from "./components/redux/store.js";
 import "./styles.css";
@@ -9,8 +10,10 @@ import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
